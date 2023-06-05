35e346bc3164c1c71d96e1ce3c01417100495108
Stela Franken Souza <stelafranken@hotmail.com>
2023-06-05 18:18:34 -0300

    alteração

--- index9.js
+++ index9.js
@@ -1,5 +1,5 @@
 /*9. Escreva um programa que pergunte a velocidade de um carro. Caso
-ultrapasse 80Km/h, exiba uma mensagem dizendo que o usuário foi
+/*ultrapasse 80Km/h, exiba uma mensagem dizendo que o usuário foi
 multado. Nesse caso, exiba o valor da multa, cobrando R$5 por cada
 km acima da velocidade permitida.*/
 
